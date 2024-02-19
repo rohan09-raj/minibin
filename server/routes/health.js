d21fee61710fb4dcfe2422b9a0e4ea9d2f685be2
rohan09-raj <rajrohan1914@gmail.com>
2024-02-19 21:52:55 +0530

    refactor: Rewrite server in go

--- server/routes/health.js
+++
@@ -1,8 +0,0 @@
-import Express from "express";
-import health from "../controllers/health.js";
-
-const router = Express.Router();
-
-router.get("/health", health.healthCheck);
-
-export default router;
