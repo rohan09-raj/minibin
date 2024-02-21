b5e6b53b0b0ed697e85dffda3a5ec41edd01e672
rohan09-raj <rajrohan1914@gmail.com>
2024-02-21 21:50:57 +0530

    feat: added link to home in header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { SUPPORTED_LANGUAGES } from "../../utils/constants";
 import styles from "./Header.module.css";
 import CustomSelect from "../CustomSelect/CustomSelect";
@@ -6,7 +7,7 @@ import CustomSelect from "../CustomSelect/CustomSelect";
 const Header = ({ isSelectVisible, onLanguageChange }) => {
   return (
     <div className={styles.header}>
-      <h1><span className={styles.header__mini}>mini</span>bin</h1>
+      <Link to="/"><h1><span className={styles.header__mini}>mini</span>bin</h1></Link>
       {isSelectVisible && (
         <CustomSelect
           options={SUPPORTED_LANGUAGES}
